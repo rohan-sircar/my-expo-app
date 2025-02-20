b8911d993fbcfd80de93ecfc3529fabedbc6dc03
Rohan Sircar <rohansircar@protonmail.com>
2025-02-20 13:55:49 +0530

    add react native reanimated configurations

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = [];
+  const plugins = ['@babel/plugin-proposal-export-namespace-from', 'react-native-reanimated/plugin',];
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,15 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const {
+    wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
+const withNWConfig = withNativeWind(config, { input: './global.css', inlineRem: 16 });
+
+
+module.exports = wrapWithReanimatedMetroConfig(withNWConfig);
