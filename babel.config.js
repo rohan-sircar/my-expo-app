d6cfebc80bf37c40a1515efdf42a7c7e10a7a590
Rohan Sircar <rohansircar@protonmail.com>
2025-02-23 13:16:34 +0530

    run yarn format

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = ['@babel/plugin-proposal-export-namespace-from', 'react-native-reanimated/plugin',];
+  const plugins = [
+    '@babel/plugin-proposal-export-namespace-from',
+    'react-native-reanimated/plugin',
+  ];
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
