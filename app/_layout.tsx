af0834002b53587002ae960c674d9aa6e60e16fe
Rohan <rohansircar@protonmail.com>
2025-02-26 01:21:12 +0530

    refactor drawer navigation structure in _layout.tsx
    Keep navigation strcuture constant while moving the isDesktop logic to be more local

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,21 @@
 import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerNavigationOptions,
+  DrawerNavigationProp,
+} from '@react-navigation/drawer';
 import { ThemeProvider as NavThemeProvider } from '@react-navigation/native';
 import 'expo-dev-client';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Platform, Pressable, SafeAreaView, View } from 'react-native';
+import { Pressable, SafeAreaView, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import '../global.css';
-import useRouteTitle from '~/lib/useRouteTitle';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ThemeToggle from '~/components/ThemeToggle';
-import { getAccentSet, useAccentColor } from '~/lib/useAccentColor';
+import { useAccentColor } from '~/lib/useAccentColor';
 import { useColorScheme, useInitialAndroidBarSync } from '~/lib/useColorScheme';
 import { useResponsiveLayout } from '~/lib/useResponsiveLayout';
 import { NAV_THEME } from '~/theme';
@@ -20,17 +23,14 @@ import { NAV_THEME } from '~/theme';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { LinearGradient } from 'expo-linear-gradient';
 import { LogoutButton } from '~/components/LogoutButton';
-import { BaseAccentGradients } from '~/theme/colors';
+import { BaseAccentGradients, SystemColors } from '~/theme/colors';
+import { NAVIGATION_CONFIG } from '~/types/navigation';
 import AuthStack from './components/AuthStack';
 import DrawerContent from './components/DrawerContent';
 import HomeTabs from './components/HomeTabs';
 import { SettingsIcon } from './components/SettingsIcon';
-import { useUserStore } from './stores/UserStore';
-import { NAVIGATION_CONFIG } from '~/types/navigation';
 import ControlsScreen from './screens/ControlsScreen';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import { NavigationContainer } from '@react-navigation/native';
+import { useUserStore } from './stores/UserStore';
 
 const Drawer = createDrawerNavigator();
 
@@ -66,95 +66,43 @@ export default function RootLayout() {
                   style={{ flex: 1 }}>
                   <SafeAreaView className="flex-1">
                     <View className="mx-auto w-full max-w-[800px] flex-1">
-                      {isDesktop ? (
-                        <Drawer.Navigator
-                          drawerContent={(props) => <DrawerContent {...props} />}
-                          screenOptions={{
-                            headerLeft: () => null, // Hide hamburger menu in desktop
-                            headerRight: () => (
-                              <View className="flex flex-row items-center gap-3 pr-2">
-                                <ThemeToggle />
-                                {loggedIn && <SettingsIcon />}
-                                {loggedIn && <LogoutButton />}
-                              </View>
-                            ),
-                            drawerType: 'permanent',
-                            drawerStyle: {
-                              width: 240,
-                              backgroundColor: colors.background,
-                            },
-                          }}>
-                          <Drawer.Screen
-                            name={NAVIGATION_CONFIG.Home.name}
-                            component={HomeTabs}
-                            options={{
-                              title: NAVIGATION_CONFIG.Home.title,
-                            }}
-                          />
-                          <Drawer.Screen
-                            name={NAVIGATION_CONFIG.Account.name}
-                            component={AuthStack}
-                            options={{
-                              title: NAVIGATION_CONFIG.Account.title,
-                            }}
-                          />
-                          <Drawer.Screen
-                            name={NAVIGATION_CONFIG.Settings.name}
-                            component={ControlsScreen}
-                            options={{
-                              title: NAVIGATION_CONFIG.Settings.title,
-                              // headerShown: true,
-                            }}
-                          />
-                        </Drawer.Navigator>
-                      ) : (
-                        <Drawer.Navigator
-                          drawerContent={(props) => <DrawerContent {...props} />}
-                          screenOptions={({ navigation }) => ({
-                            headerTitle: '',
-                            headerLeft: () => (
-                              <Pressable onPress={() => navigation.toggleDrawer()} className="ml-4">
-                                <FontAwesome name="bars" size={24} color={colors.text} />
-                              </Pressable>
-                            ),
-                            headerRight: () => (
-                              <View className="flex flex-row items-center gap-3 pr-2">
-                                <ThemeToggle />
-                                {loggedIn && <SettingsIcon />}
-                                {loggedIn && <LogoutButton />}
-                              </View>
-                            ),
-                            drawerType: 'front',
-                            drawerStyle: {
-                              backgroundColor: colors.background,
-                              width: 240,
-                            },
-                            ...SCREEN_OPTIONS,
-                          })}>
-                          <Drawer.Screen
-                            name={NAVIGATION_CONFIG.Home.name}
-                            component={HomeTabs}
-                            options={{
-                              title: NAVIGATION_CONFIG.Home.title,
-                            }}
-                          />
-                          <Drawer.Screen
-                            name={NAVIGATION_CONFIG.Account.name}
-                            component={AuthStack}
-                            options={{
-                              title: NAVIGATION_CONFIG.Account.title,
-                            }}
-                          />
-                          <Drawer.Screen
-                            name={NAVIGATION_CONFIG.Settings.name}
-                            component={ControlsScreen}
-                            options={{
-                              title: NAVIGATION_CONFIG.Settings.title,
-                              headerShown: true,
-                            }}
-                          />
-                        </Drawer.Navigator>
-                      )}
+                      <Drawer.Navigator
+                        drawerContent={(props) => <DrawerContent {...props} />}
+                        screenOptions={({ navigation }) => ({
+                          headerRight: () => (
+                            <View className="flex flex-row items-center gap-3 pr-2">
+                              <ThemeToggle />
+                              {loggedIn && <SettingsIcon />}
+                              {loggedIn && <LogoutButton />}
+                            </View>
+                          ),
+                          ...(isDesktop
+                            ? desktopDrawerProperties(colors)
+                            : mobileDrawerProperties(colors, navigation)),
+                        })}>
+                        <Drawer.Screen
+                          name={NAVIGATION_CONFIG.Home.name}
+                          component={HomeTabs}
+                          options={{
+                            title: NAVIGATION_CONFIG.Home.title,
+                          }}
+                        />
+                        <Drawer.Screen
+                          name={NAVIGATION_CONFIG.Account.name}
+                          component={AuthStack}
+                          options={{
+                            title: NAVIGATION_CONFIG.Account.title,
+                          }}
+                        />
+                        <Drawer.Screen
+                          name={NAVIGATION_CONFIG.Settings.name}
+                          component={ControlsScreen}
+                          options={{
+                            title: NAVIGATION_CONFIG.Settings.title,
+                            headerShown: isDesktop ? false : true,
+                          }}
+                        />
+                      </Drawer.Navigator>
                     </View>
                   </SafeAreaView>
                 </LinearGradient>
@@ -166,3 +114,29 @@ export default function RootLayout() {
     </>
   );
 }
+
+const desktopDrawerProperties = (colors: SystemColors): DrawerNavigationOptions => ({
+  drawerType: 'permanent',
+  drawerStyle: {
+    width: 240,
+    backgroundColor: colors.background,
+  },
+});
+
+const mobileDrawerProperties = (
+  colors: SystemColors,
+  navigation?: DrawerNavigationProp<any>
+): DrawerNavigationOptions => ({
+  headerTitle: '',
+  headerLeft: () => (
+    <Pressable onPress={() => navigation?.toggleDrawer()} className="ml-4">
+      <FontAwesome name="bars" size={24} color={colors.text} />
+    </Pressable>
+  ),
+  drawerType: 'front',
+  drawerStyle: {
+    backgroundColor: colors.background,
+    width: '80%',
+  },
+  ...SCREEN_OPTIONS,
+});
